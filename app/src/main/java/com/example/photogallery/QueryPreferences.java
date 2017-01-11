577e5f0342524971c7efc02296d634845bfc79df
Viktor <viktoryy9494@gmail.com>
2017-01-11 12:36:39 +1100

    Приложение является клиентом для ресурса Flickr. Реализованны следующие функции: просмотр недавних доступных фото (отображаются в recycleView по 100 элементов), а так же поиск с помощью SearchView.

---
+++ app/src/main/java/com/example/photogallery/QueryPreferences.java
@@ -0,0 +1,24 @@
+package com.example.photogallery;
+
+import android.content.Context;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by Виктор on 10.01.2017.
+ */
+
+public class QueryPreferences {
+    private static final String PREF_SEARCH_QUERY = "searchQuery";
+
+    public static String getStoredQuery(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getString(PREF_SEARCH_QUERY, null);
+    }
+
+    public static void setStoredQuery(Context context, String query) {
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putString(PREF_SEARCH_QUERY, query)
+                .apply();
+    }
+}
---
+++ app/src/main/java/com/example/photogallery/SingleFragmentActivity.java
@@ -0,0 +1,36 @@
+package com.example.photogallery;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+/**
+ * Created by Виктор on 15.10.2016.
+ */
+
+//этот абстрактный класс реализует интерфейс создания любого фрагмента
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+    private static final String TAG = "SingleFragmentActivity";
+
+    protected abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if(fragment == null){
+            fragment = createFragment();
+            FragmentTransaction ft =  fm.beginTransaction();
+            ft.add(R.id.fragment_container, fragment);
+            ft.commit();
+        }
+    }
+}
